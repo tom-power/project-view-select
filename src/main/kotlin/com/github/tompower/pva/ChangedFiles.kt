308a0bcc58030872fd2068fb4a31ced42634a717
Tom Power <tp21@tuta.io>
2023-07-07 16:34:30 +0100

    tidy

--- src/main/kotlin/com/github/tompower/pva/ChangedFiles.kt
+++ src/main/kotlin/com/github/tompower/pva/ChangedFiles.kt
@@ -1,22 +1,15 @@
 package com.github.tompower.pva
 
-import com.intellij.ide.DataManager
-import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
 
 
-class ChangedFiles() : AnAction(), DumbAware {
+class ChangedFiles : AnAction(), DumbAware {
     override fun actionPerformed(event: AnActionEvent) {
         val am = ActionManager.getInstance()
-        am.getAction("ActivateProjectToolWindow").actionPerformed(
-                AnActionEvent(
-                        /* inputEvent = */ null,
-                        /* dataContext = */ DataManager.getInstance().dataContext,
-                        /* place = */ ActionPlaces.UNKNOWN,
-                        /* presentation = */ Presentation(),
-                        /* actionManager = */ ActionManager.getInstance(), /* modifiers = */ 0))
-
-
+        am.getAction("ActivateProjectToolWindow").actionPerformed(event)
     }
 
     override fun update(event: AnActionEvent) {
