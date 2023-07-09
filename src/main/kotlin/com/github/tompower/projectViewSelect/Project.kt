0c01882f296e38304f8a4a8602782b0131782254
Tom Power <tp21@tuta.io>
2023-07-09 06:41:50 +0100

    tidy

--- src/main/kotlin/com/github/tompower/projectViewSelect/Project.kt
+++ src/main/kotlin/com/github/tompower/projectViewSelect/Project.kt
@@ -3,12 +3,13 @@ package com.github.tompower.projectViewSelect
 import com.intellij.ide.projectView.impl.ProjectViewPane
 import com.intellij.openapi.actionSystem.AnActionEvent
 
-
 class Project : ProjectViewSelect() {
     override fun actionPerformed(event: AnActionEvent) {
-        super.selectViewAction(
-            viewPaneId = ProjectViewPane.ID,
-            event = event
-        )
+        event.project?.let { project ->
+            super.selectViewAction(
+                viewPane = ProjectViewPane(project),
+                event = event
+            )
+        }
     }
 }
